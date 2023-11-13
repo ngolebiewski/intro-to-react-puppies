122b01fc202a53c073aa2742c892693ba80b9d5b
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-13 15:40:27 -0500

    Added Puppy list data and added to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
-
-function App() {
-
+const App = () => {
+ 
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
   return (
     <>
-      <div>
-       
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
