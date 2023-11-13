b934ac33d712f3beb40aef696c3cca7da9273a76
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-13 16:09:43 -0500

    Added featured pup logic and display to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,37 @@ import { puppyList } from './data.js'
 import { useState } from 'react'
 
 const App = () => {
- 
+
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  // console.log(puppies);
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>
+              {puppy.name}
+            </p>
           })
         }
+
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+
+
       </div>
     </>
   )
